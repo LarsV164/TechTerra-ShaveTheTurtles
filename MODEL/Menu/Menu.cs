c58d9f22adc717ff04df1f85b3a3923d827551d3
LarsV164 <lars.voncken2006@gmail.com>
2026-01-29 13:48:28 +0100

    Dier Aanmelden Class Maken

--- MODEL/Menu/Menu.cs
+++ MODEL/Menu/Menu.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TechTerra_FrontEnd.MODEL.Data;
 using TechTerra_FrontEnd.DataAccessLayer;
+using TechTerra_FrontEnd.MODEL.Menu.DierVerblijf;
 
 namespace TechTerra_FrontEnd
 {
@@ -79,7 +80,8 @@ namespace TechTerra_FrontEnd
                             OngeldigeOptie();
                             break;
                         }
-                        DierAanmelden();
+                        var dieraanmelden = new DierAanmelden();
+                        dieraanmelden.NieuwDier();
                         break;
                     case "4":
                         if (UserAccess > 2)
@@ -121,41 +123,7 @@ namespace TechTerra_FrontEnd
         }
 
 
-            private void DierAanmelden()
-        {
-            Console.Clear();
-            Console.WriteLine("=== NIEUW DIER AANMELDEN ===");
-
-            // Vragen om invoer 
-            Console.Write("Naam dier: ");
-            string naam = Console.ReadLine();
-
-            Console.Write("Soort: ");
-            string soort = Console.ReadLine();
-
-            Console.Write("Verblijf ID (nummer): ");
-            int vId = int.Parse(Console.ReadLine());
-
-            Console.Write("Geboortedatum (dd-mm-jjjj): ");
-            DateTime datum = DateTime.Parse(Console.ReadLine());
-
-          // Opslaan via de DAL
-            try
-            {
-                DAL dal = new DAL();
-                dal.VoegDierToe(naam, soort, vId, datum);
-                Console.WriteLine("Gelukt! Het dier is opgeslagen."); // Bevestiging 
-            }
-            catch (Exception ex)
-            {
-                // als het misgaat
-                Console.WriteLine("Fout: ");
-            }
-
-            Console.WriteLine("Druk op een toets voor het menu...");
-            Console.ReadKey();
-            Console.Clear();
-        }
+            
         
 
         private void DeurMeldingen()
